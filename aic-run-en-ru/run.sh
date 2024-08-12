5821c90fa60c6f747a6024714fe3d4b704af0284
lack-of-purpose <35802031@o365.cuni.cz>
2024-08-12 19:57:19 +0200

    keep secret

--- aic-run-en-ru/run.sh
+++ aic-run-en-ru/run.sh
@@ -8,6 +8,6 @@
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip3 install -U optuna transformers datasets pandas scikit-learn hyperopt xgboost SentencePiece wandb unbabel-comet lightning torchmetrics
 pip3 install accelerate -U
-export WANDB_API_KEY=52417aeb280e54ec9b4c205a8979c18f4456a44e
+export WANDB_API_KEY=
 wandb login
 python3 xlm-r-ft-optuna.py
