5ef2629fde74386a334493bc9d29fc3d19acf1d0
lack-of-purpose <35802031@o365.cuni.cz>
2025-04-21 18:45:57 +0200

    update run.sh

--- aic-run-en-ru/run.sh
+++ aic-run-en-ru/run.sh
@@ -6,8 +6,8 @@
 #SBATCH -e error.txt
 
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip3 install -U optuna transformers datasets pandas scikit-learn hyperopt xgboost SentencePiece wandb unbabel-comet lightning torchmetrics
+pip3 install -U optuna transformers datasets pandas scikit-learn hyperopt SentencePiece wandb
 pip3 install accelerate -U
 export WANDB_API_KEY=
 wandb login
-python3 xlm-r-ft-optuna.py
+python3 xlm-r-fine-tuning.py
